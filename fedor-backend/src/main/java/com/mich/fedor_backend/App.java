2f8e97f6fbc4bd7869d60de5cdac17650fbdcbde
Michael Kuklin <zolozavr@gmail.com>
2017-06-01 15:31:47 +0300

    Project Setup Complete

---
+++ fedor-backend/src/main/java/com/mich/fedor_backend/App.java
@@ -0,0 +1,13 @@
+package com.mich.fedor_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
