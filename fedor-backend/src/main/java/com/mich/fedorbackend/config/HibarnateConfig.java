30fead2041f8b1912fe1ac14eff752b0fd786f75
Michael Kuklin <zolozavr@gmail.com>
2017-06-08 14:17:22 +0300

    Hibernate config completed

--- fedor-backend/src/main/java/com/mich/fedorbackend/config/HibarnateConfig.java
+++ fedor-backend/src/main/java/com/mich/fedorbackend/config/HibarnateConfig.java
@@ -1,16 +1,63 @@
 package com.mich.fedorbackend.config;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@ComponentScan(basePackages="com.mich.fedorbackend.dto")
+@ComponentScan(basePackages = "com.mich.fedorbackend.dto")
 @EnableTransactionManagement
+// MySQL DATABASE CONFIG
 public class HibarnateConfig {
-private final static String DATABASE_URL = "jdbc:mysql://localhost:3306/root";
-private final static String DATABASE_DRIVER = "com.mysql.jdbc.Driver";
-private final static String DATABASE_DIALECT = "org.hibernate.dialect.MySQLDialect";
-private final static String DATABASE_USERNAME = "root";
-private final static String DATABASE_PASSWORD = "123456";
+	private final static String DATABASE_URL = "jdbc:mysql://localhost:3306/root";
+	private final static String DATABASE_DRIVER = "com.mysql.jdbc.Driver";
+	private final static String DATABASE_DIALECT = "org.hibernate.dialect.MySQLDialect";
+	private final static String DATABASE_USERNAME = "root";
+	private final static String DATABASE_PASSWORD = "123456";
+
+	@Bean
+	private DataSource getDataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		// Database connection
+		dataSource.setDriverClassName(DATABASE_DRIVER);
+		dataSource.setUrl(DATABASE_URL);
+		dataSource.setUsername(DATABASE_USERNAME);
+		dataSource.setPassword(DATABASE_PASSWORD);
+
+		return dataSource;
+	}
+
+	@Bean
+	public SessionFactory getSessionFactory(DataSource dataSource) {
+		LocalSessionFactoryBuilder builder = new LocalSessionFactoryBuilder(dataSource);
+		builder.addProperties(getHibernateProperties());
+		builder.scanPackages("com.mich.fedorbackend.dto");
+
+		return builder.buildSessionFactory();
+	}
+
+	// Return hibernate properties
+	private Properties getHibernateProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", DATABASE_DIALECT);
+		properties.put("hibernate.show_sql", "true");
+		properties.put("hibernate.format_sql", "true");
+		return null;
+	}
+
+	// Transaction manager bean
+	@Bean
+	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+		return transactionManager;
+	}
 }
